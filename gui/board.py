048977544911be689803bfa2d085929ff55c1fb6
chris <papadopoulos.chris@hotmail.com>
2022-03-11 00:29:53 +0200

    Μετατροπή σε αντικειμενοστραφή προγραμματισμό, η μέθοδος για το κλικ δεν λειτουργεί ακόμη.

--- gui/board.py
+++ gui/board.py
@@ -1,77 +1,115 @@
 from textwrap import fill
 from tkinter import *
 
-board = Tk()
-board.title("Scrabble")
-board.geometry("1520x980")
-board.configure(background="lightgrey")
-canvas = Canvas(board, width=1920, height=1080)
-canvas.pack()
+class Board(Tk):
+    def __init__(self):
+        super().__init__()
 
-rects = {}
+        self.title("Scrabble")
+        self.geometry("1520x980")
+        self.configure(background="lightgrey")
+        self.canvas = Canvas(self, width=1920, height=1080)
+        self.canvas.pack()
 
-x1 = 0 
-y1 = 0
-height = 50 
-width = 50
+        self.rects = {}
 
-for x in range(15):
-    y1+=50
-    width += 50
-    for y in range(15):
-        x1+=50
-        height += 50
-        if ((x==0 and y == 0) or (x==0 and y == 7) or (x==0 and y == 14) or (x==7 and y == 0) or (x==7 and y == 14) or (x==14 and y == 0) or (x==14 and y == 7) or (x==14 and y == 14)):
-            #keli gia leksi triplis aksias
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fill="red")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="   ΛΕΞΗ\nΤΡΙΠΛΗΣ\n   ΑΞΙΑΣ")
-        elif((x==1 and y == 5) or (x==1 and y == 9) or (x==5 and y == 1) or (x==5 and y == 5) or (x==5 and y == 9) or (x==5 and y == 13) or (x==9 and y == 1) or (x==9 and y == 5)
-            or (x==9 and y == 9) or (x==9 and y == 13) or (x==13 and y == 5) or (x==13 and y == 9)):
-            #keli gia gramma triplis aksias
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fill="blue")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="ΓΡΑΜΜΑ\nΤΡΙΠΛΗΣ\n   ΑΞΙΑΣ")
-        elif((x==0 and y == 3) or (x==0 and y == 11) or (x==2 and y == 6) or (x==2 and y == 8) or (x==3 and y == 0) or (x==3 and y == 7) or (x==3 and y == 14) or (x==6 and y == 2)
-            or (x==6 and y == 6) or (x==6 and y == 8) or (x==6 and y == 12) or (x==7 and y == 3) or (x==7 and y == 11) or (x==8 and y == 2) or (x==8 and y == 6) or (x==8 and y == 8) or (x==8 and y == 12)
-            or (x==11 and y == 0) or (x==11 and y == 7) or (x==11 and y == 14) or (x==12 and y == 6) or (x==12 and y == 8) or (x==14 and y == 3) or (x==14 and y == 11)):
-            #keli gia gramma diplis aksias
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fill="light blue")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="ΓΡΑΜΜΑ\n ΔΙΠΛΗΣ\n   ΑΞΙΑΣ")
-        elif((x==1 and y == 1) or (x==1 and y == 13) or (x==2 and y == 2) or (x==2 and y == 12) or (x==3 and y == 3) or (x==3 and y == 11) or (x==4 and y == 4) or (x==4 and y == 10)
-            or (x==10 and y == 4) or (x==10 and y == 10) or (x==11 and y == 3) or (x==11 and y == 11) or (x==12 and y == 2) or (x==12 and y == 12) or (x==13 and y == 1) or (x==13 and y == 13) ):
-            #keli gia leksi diplis aksias
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fill="light pink")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="   ΛΕΞΗ\n ΔΙΠΛΗΣ\n   ΑΞΙΑΣ")
-        elif(x==7 and y == 7):
-            #kentriko keli
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fill="light pink")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="ΑΡΧΗ")
-        else:
-            #ta upoloipa klasika kelia
-            tile_rect = canvas.create_rectangle(x1, y1, height, width, fil="green")
-            tile_txt = canvas.create_text((x1 + height)/ 2, (y1 + width)/2, anchor='center', text="")
-        rects[(x,y)] = [tile_rect, tile_txt]
-        print(x1, y1, height, width)
+        self.x1 = 0 
+        self.y1 = 0
+        self.height = 50 
+        self.width = 50
 
-    x1= 0
-    height= 50
+        for x in range(15):
+            self.y1+=50
+            self.width += 50
+            for y in range(15):
+                self.x1+=50
+                self.height += 50
+                if ((x==0 and y == 0) or (x==0 and y == 7) or (x==0 and y == 14) or (x==7 and y == 0) or (x==7 and y == 14) or (x==14 and y == 0) or (x==14 and y == 7) or (x==14 and y == 14)):
+                    #keli gia leksi triplis aksias
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fill="red", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="   ΛΕΞΗ\nΤΡΙΠΛΗΣ\n   ΑΞΙΑΣ", tags= f"{x},{y}")
+                elif((x==1 and y == 5) or (x==1 and y == 9) or (x==5 and y == 1) or (x==5 and y == 5) or (x==5 and y == 9) or (x==5 and y == 13) or (x==9 and y == 1) or (x==9 and y == 5)
+                    or (x==9 and y == 9) or (x==9 and y == 13) or (x==13 and y == 5) or (x==13 and y == 9)):
+                    #keli gia gramma triplis aksias
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fill="blue", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="ΓΡΑΜΜΑ\nΤΡΙΠΛΗΣ\n   ΑΞΙΑΣ", tags= f"{x},{y}")
+                elif((x==0 and y == 3) or (x==0 and y == 11) or (x==2 and y == 6) or (x==2 and y == 8) or (x==3 and y == 0) or (x==3 and y == 7) or (x==3 and y == 14) or (x==6 and y == 2)
+                    or (x==6 and y == 6) or (x==6 and y == 8) or (x==6 and y == 12) or (x==7 and y == 3) or (x==7 and y == 11) or (x==8 and y == 2) or (x==8 and y == 6) or (x==8 and y == 8) or (x==8 and y == 12)
+                    or (x==11 and y == 0) or (x==11 and y == 7) or (x==11 and y == 14) or (x==12 and y == 6) or (x==12 and y == 8) or (x==14 and y == 3) or (x==14 and y == 11)):
+                    #keli gia gramma diplis aksias
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fill="light blue", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="ΓΡΑΜΜΑ\n ΔΙΠΛΗΣ\n   ΑΞΙΑΣ", tags= f"{x},{y}")
+                elif((x==1 and y == 1) or (x==1 and y == 13) or (x==2 and y == 2) or (x==2 and y == 12) or (x==3 and y == 3) or (x==3 and y == 11) or (x==4 and y == 4) or (x==4 and y == 10)
+                    or (x==10 and y == 4) or (x==10 and y == 10) or (x==11 and y == 3) or (x==11 and y == 11) or (x==12 and y == 2) or (x==12 and y == 12) or (x==13 and y == 1) or (x==13 and y == 13) ):
+                    #keli gia leksi diplis aksias
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fill="light pink", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="   ΛΕΞΗ\n ΔΙΠΛΗΣ\n   ΑΞΙΑΣ", tags= f"{x},{y}")
+                elif(x==7 and y == 7):
+                    #kentriko keli
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fill="light pink", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="ΑΡΧΗ", tags= f"{x},{y}")
+                else:
+                    #ta upoloipa klasika kelia
+                    tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fil="green", tags= f"{x},{y}")
+                    tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="", tags= f"{x},{y}")
+                self.rects[(x,y)] = [tile_rect, tile_txt]
 
-#dokimi gia pithani proseggisi tropopoiisis xromatos kai keimenou sto canvas/keli
-#canvas.itemconfigure(rects[(1,1)][1],font=("Arial", 35), anchor='center', text=S)
-#canvas.itemconfigure(rects[(1,1)][0], fill="white")
+            self.x1= 0
+            self.height= 50
 
-player_letters = {}
-p_x1 = 250
-p_y1 = 850
-p_height = 300
-p_width = 900
+        #dokimi gia pithani proseggisi tropopoiisis xromatos kai keimenou sto canvas/keli
+        #canvas.itemconfigure(rects[(1,1)][1],font=("Arial", 35), anchor='center', text=S)
+        #canvas.itemconfigure(rects[(1,1)][0], fill="white")
 
-test_let = ["Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η"]
+        self.player_letters = {}
+        self.p_x1 = 250
+        self.p_y1 = 850
+        self.p_height = 300
+        self.p_width = 900
 
-for i in range(7):
-    player_tile_rect = canvas.create_rectangle(p_x1, p_y1, p_height, p_width, fill="white")
-    player_tile_txt = canvas.create_text((p_x1 + p_height)/ 2, (p_y1 + p_width)/2, anchor='center', text=f"{test_let[i]}")
-    player_letters[i] = [player_tile_rect, player_tile_txt]
-    p_x1 += 50
-    p_height += 50
+        test_let = ["Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η"]
 
-board.mainloop()
+        for i in range(7):
+            player_tile_rect = self.canvas.create_rectangle(self.p_x1, self.p_y1, self.p_height, self.p_width, fill="white", tags= f"{i}")
+            player_tile_txt = self.canvas.create_text((self.p_x1 + self.p_height)/ 2, (self.p_y1 + self.p_width)/2, anchor='center', text=f"{test_let[i]}", tags= f"{i}")
+            self.player_letters[i] = [player_tile_rect, player_tile_txt]
+            self.p_x1 += 50
+            self.p_height += 50
+
+
+        def on_click(self, event, transfer = False):
+            data = []
+            if transfer == False:
+                new_data = self.proto_keli(event, data)
+                transfer = True
+            
+            if transfer == True:
+                self.deutero_keli(event, new_data)
+                transfer = False
+
+        def proto_keli(self, event, data):
+            item1 = self.canvas.find_closest(event.x, event.y)
+            tags1 = self.canvas.itemcget(item1, "tags")
+            if tags1 in self.player_letters:
+                color = self.canvas.itemcget(tags1[0], "fill")
+                text = self.canvas.itemcget(tags1[1], "text")
+                data = [color, text]
+            return data
+
+
+        def deutero_keli(self, event, new_data):
+            item2 = self.canvas.find_closest(event.x, event.y)
+            tags2 = self.canvas.itemcget(item2, "tags")
+            if tags2 in self.rects:
+                tags2.itemconfigure(font=("Arial", 35), anchor='center', text= f"{new_data[1]}")
+                tags2.itemconfigure(fill=f"{new_data[0]}")
+        
+
+
+        self.canvas.bind('<Button-1>', on_click)
+        self.canvas.bind('<Button-1>', proto_keli)
+        self.canvas.bind('<Button-1>', deutero_keli)
+
+if __name__ == "__main__":
+    board = Board()
+    board.mainloop()
