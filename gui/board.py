4a55be6f9b6d2391a1bbc59d045c2b7f0fe3e19d
chris <papadopoulos.chris@hotmail.com>
2022-03-16 22:29:23 +0200

    Ολοκληρώθηκε ένα λειτουργικό πρωτότυπο για την τοποθέτηση γραμμάτων μέσω ποντικιού.

--- gui/board.py
+++ gui/board.py
@@ -10,7 +10,8 @@ class Board(Tk):
         self.configure(background="lightgrey")
         self.canvas = Canvas(self, width=1920, height=1080)
         self.canvas.pack()
-        
+        self.transfer = False
+        self.transfer_letter = ""
 
         self.rects = {}
 
@@ -53,14 +54,11 @@ class Board(Tk):
                     #ta upoloipa klasika kelia
                     tile_rect = self.canvas.create_rectangle(self.x1, self.y1, self.height, self.width, fil="green", tags= f"{x},{y}")
                     tile_txt = self.canvas.create_text((self.x1 + self.height)/ 2, (self.y1 + self.width)/2, anchor='center', text="", tags= f"{x},{y}")
-                self.rects[(x,y)] = [tile_rect, tile_txt]
+                self.rects[(f"{x},{y}")] = [tile_rect, tile_txt]
+                
 
             self.x1= 0
             self.height= 50
-
-        #dokimi gia tropopoiisi xromatos kai keimenou sto canvas/keli
-        #canvas.itemconfigure(rects[(1,1)][1],font=("Arial", 35), anchor='center', text=S)
-        #canvas.itemconfigure(rects[(1,1)][0], fill="white")
 
         self.player_letters = {}
         self.p_x1 = 250
@@ -68,7 +66,7 @@ class Board(Tk):
         self.p_height = 300
         self.p_width = 900
 
-        test_let = ["Α", "Β", "Γ", "Δ", "Ε", "Ζ", "Η"]
+        test_let = ["Ε", "Χ", "Τ", "Π", "Ε", "Ο", "Υ"]
 
         for i in range(7):
             player_tile_rect = self.canvas.create_rectangle(self.p_x1, self.p_y1, self.p_height, self.p_width, fill="white", tags= f"{i}")
@@ -79,56 +77,28 @@ class Board(Tk):
 
 
         self.canvas.bind('<Button-1>', self.on_click)
-        self.canvas.bind('<Button-1>', self.proto_keli)
-        #self.canvas.bind('<Button-1>', self.deutero_keli)
 
     def on_click(self, event):
-        transfer = False
-        data = []
         
-        if transfer == False:
-            data.append(self.proto_keli(event))
-            transfer = True
-            print(data)
-            print(transfer)
-        
-        print(data)
-        print(transfer)
-
-
-        # if transfer == True:
-        #     self.deutero_keli(event, data)
-        #     self.transfer = False
-                   
-
-    def proto_keli(self, event):
-        item1 = self.canvas.find_closest(event.x, event.y)
-        tags1 = self.canvas.itemcget(item1, "tags")
-        #print(tags1)
-        if int(tags1[0]) in self.player_letters:
-            tags1 = int(tags1.replace(" current", ""))
-            color = self.canvas.itemcget(self.player_letters[tags1][0], "fill")
-            print(color)
-            text = self.canvas.itemcget(self.player_letters[tags1][1], "text")
-            #print(text)
-            data = [color, text]
-            return data
-            
-    
-
-    # def deutero_keli(self, event, data):
-    #     item2 = self.canvas.find_closest(event.x, event.y)
-    #     tags2 = self.canvas.itemcget(item2, "tags")
-    #     print(tags2)
-    #     if tags2 in self.rects:
-    #         tags2 = int(tags2.split())
-    #         self.canvas.itemconfigure(self.rects[(tags2[0],tags2[1])], font=("Arial", 35), anchor='center', text= data[0])
-    #         self.canvas.itemconfigure(self.rects[(tags2[0],tags2[1])], fill=f"{data[1]}")
-             
-        
-
-
-
+        if self.transfer == False:
+            item1 = self.canvas.find_closest(event.x, event.y)
+            tags1 = self.canvas.itemcget(item1, "tags")
+            print(tags1)
+            if int(tags1[0]) in self.player_letters:
+                tags1 = int(tags1.replace(" current", ""))
+                color = self.canvas.itemcget(self.player_letters[tags1][0], "fill")
+                print(color)
+                self.transfer_letter = self.canvas.itemcget(self.player_letters[tags1][1], "text")
+                self.transfer = True
+        else:
+            item2 = self.canvas.find_closest(event.x, event.y)
+            tags2 = self.canvas.itemcget(item2, "tags")
+            tags2 = tags2.replace(" current", "")
+            print(tags2)
+            if tags2 in self.rects:
+                self.canvas.itemconfigure(self.rects[tags2][1], font=("Arial", 35), anchor='center', text= self.transfer_letter)
+                self.canvas.itemconfigure(self.rects[tags2][0], fill= "white")
+                self.transfer = False
 
 if __name__ == "__main__":
     board = Board()
